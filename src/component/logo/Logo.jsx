c2e4b19ec971fd23f0e8bb423b18510d5a0878dc
Akriti Ghosh <akritighosh98@gmail.com>
2024-07-07 12:57:28 +0530

    logo components

---
+++ src/component/logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logoPath from "./logo.svg";
+import './logo.css'
+
+const Logo = () => {
+  return <img src={logoPath} className="logo" />;
+};
+
+export default Logo;
