8cf8988a5baee1a74110aaa4a52a9d5186d62d23
Akriti Ghosh <akritighosh98@gmail.com>
2024-07-07 11:42:43 +0530

    tailwind installed and configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="w-screen h-screen flex flex-col gap-10 items-center justify-center">
+      <h1 className="font-serif text-4xl">React library</h1>
+      <button className="bg-black rounded-3xl h-10 py-2 px-4 text-white ">
+        Start here
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
