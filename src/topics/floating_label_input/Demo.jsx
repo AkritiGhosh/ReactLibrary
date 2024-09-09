510ad44ba434abed25de36f7685c26f899a6f5f0
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-09 14:57:25 +0530

    css for focus - floating effect on label

--- src/topics/floating_label_input/Demo.jsx
+++ src/topics/floating_label_input/Demo.jsx
@@ -6,10 +6,16 @@ const FLIDemo = () => {
       <div className="w-full relative">
         <input
           type="text"
-          className="w-full h-10 rounded-lg text-base px-2 bg-transparent border-2 border-black"
+          required
+          className="w-full h-10 rounded-lg text-base px-2 bg-transparent border-2 border-black focus:border-teal-500 !outline-0 peer"
           id="floating_label_input"
         />
-        <label className="text-base absolute left-2 top-2/4 -translate-y-1/2 pointer-events-none duration-300" htmlFor="floating_label_input">
+        <label
+          className="text-base absolute left-2 top-2/4 -translate-y-1/2 pointer-events-none duration-300 
+          peer-focus:text-xs peer-focus:top-0 peer-focus:px-1 peer-focus:bg-white 
+          peer-valid:text-xs peer-valid:top-0 peer-valid:px-1 peer-valid:bg-white"
+          htmlFor="floating_label_input"
+        >
           Enter value here
         </label>
       </div>
