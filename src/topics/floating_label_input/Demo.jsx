a24a47bafba6b83377479a94cdb1dae76b6620f7
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-09 14:47:40 +0530

    label standard css

--- src/topics/floating_label_input/Demo.jsx
+++ src/topics/floating_label_input/Demo.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const FLIDemo = () => {
   return (
     <section>
-      <div className="w-full">
+      <div className="w-full relative">
         <input
           type="text"
-          className="w-full h-10 rounded-lg text-base px-2 bg-transparent border-2 border-teal-500"
+          className="w-full h-10 rounded-lg text-base px-2 bg-transparent border-2 border-black"
           id="floating_label_input"
         />
-        <label className="text-xs" htmlFor="floating_label_input">
+        <label className="text-base absolute left-2 top-2/4 -translate-y-1/2 pointer-events-none duration-300" htmlFor="floating_label_input">
           Enter value here
         </label>
       </div>
