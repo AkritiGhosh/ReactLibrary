a6daaa97e8e2d326a0350a54018d75a9e9dccaf9
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-16 11:58:04 +0530

    routes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
-import RoutesPage from "./page/Routes";
+import MainLayout from "./layout/MainLayout";
+import { tabs } from "./lib/constant";
+import Main from "./page/Main";
+import CodeLayout from "./layout/CodeLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/routes" element={<RoutesPage />} />
+        <Route path="/main" element={<MainLayout default={true} />}>
+          <Route path=":page" element={<Main />}>
+            <Route path={tabs.ARTICLE} element={<Main />} />
+            <Route path={tabs.CODE} element={<CodeLayout />} />
+            <Route path={tabs.DEMO} element={<Main />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
