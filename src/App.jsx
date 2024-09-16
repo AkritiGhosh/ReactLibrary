56a83925450b56f06a1783761972453dc9b5b74a
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-16 12:18:57 +0530

    routes updated for tabs

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import MainLayout from "./layout/MainLayout";
 import { tabs } from "./lib/constant";
 import Main from "./page/Main";
 import CodeLayout from "./layout/CodeLayout";
+import ArticleLayout from "./layout/ArticleLayout";
+import DemoLayout from "./layout/DemoLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/main" element={<MainLayout default={true} />}>
+        <Route path="/main" element={<MainLayout selected={false} />}>
           <Route path=":page" element={<Main />}>
-            <Route path={tabs.ARTICLE} element={<Main />} />
-            <Route path={tabs.CODE} element={<CodeLayout />} />
-            <Route path={tabs.DEMO} element={<Main />} />
+            <Route
+              path={tabs.ARTICLE.toLowerCase()}
+              element={<ArticleLayout />}
+            />
+            <Route path={tabs.CODE.toLowerCase()} element={<CodeLayout />} />
+            <Route path={tabs.DEMO.toLowerCase()} element={<DemoLayout />} />
           </Route>
         </Route>
       </Routes>
