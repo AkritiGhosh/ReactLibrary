d15cc6e135652cff9916000b324181c04073f022
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-16 11:58:59 +0530

    data and constant defined

---
+++ src/data/data.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Dropdown from "../topics/dropdown/Demo";
+import FLIDemo from "../topics/floating_label_input/Demo";
+
+const data = [
+  {
+    name: "Dropdown",
+    id: "dropdown",
+    demoTab: <Dropdown />,
+    codeTab: <></>,
+    articleTab: <></>,
+  },
+  {
+    name: "Floating Label for Input",
+    id: "floating_label",
+    demoTab: <FLIDemo />,
+    codeTab: <></>,
+    articleTab: <></>,
+  },
+];
+
+export default data;
---
+++ src/lib/constant.jsx
@@ -0,0 +1,5 @@
+export const tabs = {
+  ARTICLE: "Explanation",
+  CODE: "Code",
+  DEMO: "Demo",
+};
