b277edfcb0071fec92cb71ef2997a4247685def5
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-16 12:20:24 +0530

    tab switch and navigation

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <main className="w-screen h-screen flex flex-col gap-10 items-center justify-center">
       <h1 className="font-serif text-4xl">React library</h1>
-      <button className="bg-black rounded-3xl h-10 py-2 px-4 text-white ">
-        Start here
-      </button>
+      <Link to={"/main"}>
+        <button className="bg-black rounded-3xl h-10 py-2 px-4 text-white ">
+          Start here
+        </button>
+      </Link>
     </main>
   );
-}
+};
 
-export default Home
+export default Home;
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,7 +1,47 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useMemo, useState } from "react";
+import {
+  Navigate,
+  Outlet,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { tabs } from "../lib/constant";
+import data from "../data/data";
 
 const Main = () => {
+  const loc = useLocation();
+  const nav = useNavigate();
+  const currentPage = useParams().page;
+  const metadata = data.filter((iter) => iter.id == currentPage)[0];
+
+  const [activeTab, setActiveTab] = useState(
+    loc.pathname?.split("/")?.length > 2
+      ? loc.pathname?.split("/").at(-1)
+      : tabs.ARTICLE.toLowerCase()
+  );
+
+  const switchTabs = (e) => {
+    let tabName = e.target.id;
+    if (activeTab != tabName) {
+      setActiveTab(tabName);
+      nav(tabName.toLowerCase());
+    }
+  };
+
+  const tabContent = useMemo(() => {
+    switch (activeTab) {
+      case tabs.ARTICLE:
+        return metadata?.articleTab;
+      case tabs.CODE:
+        return metadata?.codeTab;
+      case tabs.DEMO:
+        return metadata?.demoTab;
+      default:
+        return metadata?.articleTab;
+    }
+  }, [activeTab]);
+
   return (
     <div className="lg:w-3/4 grow w-full lg:h-screen h-auto overflow-y-auto relative">
       <div id="topBanner" className="fixed top-0 right-0 "></div>
