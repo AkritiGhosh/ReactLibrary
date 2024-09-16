8c71ff848f4d1b84a84f77e45685ee14f77f6103
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-16 12:18:29 +0530

    tab layout files added

---
+++ src/layout/ArticleLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArticleLayout = () => {
+  return (
+    <div>ArticleLayout</div>
+  )
+}
+
+export default ArticleLayout
---
+++ src/layout/DemoLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DemoLayout = () => {
+  return (
+    <div>DemoLayout</div>
+  )
+}
+
+export default DemoLayout
