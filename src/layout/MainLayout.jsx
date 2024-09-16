02702b2408d5ed9f78c5d8ee916bd36191c9bfc1
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-16 12:20:02 +0530

    show 1st data if nothing is selected

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,39 +1,21 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect } from "react";
 import Sidebar from "../component/Sidebar";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import data from "../data/data";
 import { tabs } from "../lib/constant";
 
-const MainLayout = ({ children }) => {
-  const loc = useLocation();
-  const currentPage = loc.pathname.split("/").at(-2);
-  const [activeTab, setActiveTab] = useState("explanation");
-
-  const metadata = data.filter((iter) => iter.id == currentPage)[0];
-  console.log(metadata);
-  const switchTabs = (e) => {
-    let tabName = e.target.id;
-    if (activeTab != tabName) setActiveTab(tabName);
-  };
-
-  const tabContent = useMemo(() => {
-    switch (activeTab) {
-      case tabs.ARTICLE:
-        return metadata?.articleTab;
-      case tabs.CODE:
-        return metadata?.codeTab;
-      case tabs.DEMO:
-        return metadata?.demoTab;
-      default:
-        return metadata?.articleTab;
-    }
-  }, [activeTab]);
+const MainLayout = ({ selected = false }) => {
+  const nav = useNavigate();
+  useEffect(() => {
+    if (!selected)
+      nav(data[0].id + "/" + tabs.ARTICLE.toLowerCase(), { replace: true });
+  }, [selected]);
 
   return (
     <main className="w-screen h-screen flex flex-col lg:flex-row items-center justify-center">
       {/* Sidebar component */}
       <div className="lg:w-1/4 w-full lg:h-screen h-auto lg:overflow-y-auto bg-black">
-        <Sidebar activePage={currentPage} />
+        <Sidebar />
       </div>
       {/* Content component */}
       <Outlet />
