4d58cbf3476ba6564e04c19e975f91cd236ad592
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-16 11:58:41 +0530

    main page - layout and tabs defined

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Sidebar from "../component/Sidebar";
-import Content from "../component/Content";
+import { Outlet, useLocation } from "react-router-dom";
+import data from "../data/data";
+import { tabs } from "../lib/constant";
 
 const MainLayout = ({ children }) => {
+  const loc = useLocation();
+  const currentPage = loc.pathname.split("/").at(-2);
+  const [activeTab, setActiveTab] = useState("explanation");
+
+  const metadata = data.filter((iter) => iter.id == currentPage)[0];
+  console.log(metadata);
+  const switchTabs = (e) => {
+    let tabName = e.target.id;
+    if (activeTab != tabName) setActiveTab(tabName);
+  };
+
+  const tabContent = useMemo(() => {
+    switch (activeTab) {
+      case tabs.ARTICLE:
+        return metadata?.articleTab;
+      case tabs.CODE:
+        return metadata?.codeTab;
+      case tabs.DEMO:
+        return metadata?.demoTab;
+      default:
+        return metadata?.articleTab;
+    }
+  }, [activeTab]);
+
   return (
     <main className="w-screen h-screen flex flex-col lg:flex-row items-center justify-center">
       {/* Sidebar component */}
       <div className="lg:w-1/4 w-full lg:h-screen h-auto lg:overflow-y-auto bg-black">
-        <Sidebar />
+        <Sidebar activePage={currentPage} />
       </div>
       {/* Content component */}
-      <div className="lg:w-3/4 grow w-full lg:h-screen h-auto overflow-y-auto">
-        <Content />
-      </div>
+      <Outlet />
     </main>
   );
 };
