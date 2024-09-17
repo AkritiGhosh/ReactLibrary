c6980495a0521f3dd3a81d405c0a1b9d8ecaf3be
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-17 11:02:31 +0530

    sidebar - styling + list items added

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Logo from "./logo/Logo";
+import data from "../data/data";
+import { Link } from "react-router-dom";
+import { tabs } from "../lib/constant";
 
 const Sidebar = () => {
   return (
-    <div className="header">
-      <Logo />
+    <div className="w-full flex flex-col py-4 gap-4">
+      <div className="flex gap-4 px-4 items-center">
+        <Logo />
+        <h1 className="text-cyan-400 text-xl font-medium">React Library</h1>
+      </div>
+      <div className="flex flex-col divide-y divide-gray-800">
+        {data?.map((item) => (
+          <Link to={"/main/" + item?.id + "/" + tabs.ARTICLE}>
+            <div className={`w-full text-white font-base px-4 py-2 hover:bg-cyan-300/40`}>{item?.name}</div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
