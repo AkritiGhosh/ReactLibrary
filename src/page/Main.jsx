1bdb2c060e362321373f7f8c1d02dabfb36cb7a6
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-17 10:30:09 +0530

    top banner and tabs - styling complete

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   Navigate,
   Outlet,
@@ -16,10 +16,14 @@ const Main = () => {
   const metadata = data.filter((iter) => iter.id == currentPage)[0];
 
   const [activeTab, setActiveTab] = useState(
-    loc.pathname?.split("/")?.length > 2
+    loc.pathname?.split("/")?.length >= 4
       ? loc.pathname?.split("/").at(-1)
       : tabs.ARTICLE.toLowerCase()
   );
+  console.log(loc.pathname?.split("/")?.at(-1), activeTab == tabs.ARTICLE);
+  useEffect(() => {
+    document.title = metadata?.name + " - React Library";
+  }, [metadata]);
 
   const switchTabs = (e) => {
     let tabName = e.target.id;
@@ -44,15 +48,50 @@ const Main = () => {
 
   return (
     <div className="lg:w-3/4 grow w-full lg:h-screen h-auto overflow-y-auto relative">
-      <div id="topBanner" className="fixed top-0 right-0 "></div>
-      <div id="tabs" className="">
-        <span onClick={switchTabs} className={``} id={tabs.ARTICLE}>
+      <div
+        id="topBanner"
+        className="w-full sticky top-0 right-0 flex flex-col gap-4 p-4"
+      >
+        <h1 className="text-4xl font-bold text-black">{metadata?.heading}</h1>
+        <h2 className="text-base italic font-semibold text-gray-600">
+          {metadata?.subheading}
+        </h2>
+      </div>
+      <div
+        id="tabs"
+        className="px-4 py-1 font-semibold text-gray-700 text-xl border-b-2 border-solid relative"
+      >
+        <span
+          onClick={switchTabs}
+          className={`px-3 py-1 border-b-2 border-solid hover:border-blue-500  ${
+            tabs.ARTICLE?.toLowerCase() == activeTab
+              ? "text-blue-800 border-blue-800"
+              : "border-transparent"
+          }`}
+          id={tabs.ARTICLE}
+        >
           Explanation
         </span>
-        <span onClick={switchTabs} className={``} id={tabs.CODE}>
+        <span
+          onClick={switchTabs}
+          className={`px-3 py-1 border-b-2 border-solid hover:border-blue-500  ${
+            tabs.CODE?.toLowerCase() == activeTab
+              ? "text-blue-800 border-blue-800"
+              : "border-transparent"
+          }`}
+          id={tabs.CODE}
+        >
           Code
         </span>
-        <span onClick={switchTabs} className={``} id={tabs.DEMO}>
+        <span
+          onClick={switchTabs}
+          className={`px-3 py-1 border-b-2 border-solid hover:border-blue-500  ${
+            tabs.DEMO?.toLowerCase() == activeTab
+              ? "text-blue-800 border-blue-800"
+              : "border-transparent"
+          }`}
+          id={tabs.DEMO}
+        >
           Demo
         </span>
       </div>
