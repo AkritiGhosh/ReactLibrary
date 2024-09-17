70dafdd9866f304e6dd4413bebecf96d54ae6ce1
Akriti Ghosh <akritighosh98@gmail.com>
2024-09-17 10:29:52 +0530

    data updated for heading and subheading

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -5,6 +5,9 @@ import FLIDemo from "../topics/floating_label_input/Demo";
 const data = [
   {
     name: "Dropdown",
+    heading: "Custom Dropdown Button",
+    subheading:
+      "Create a dropdown button. The element is a simple button, which when clicked opens up a list of options to select. The list closes if (1) an option from list is selected; (2) area outside the dropdown is clicked. ",
     id: "dropdown",
     demoTab: <Dropdown />,
     codeTab: <></>,
@@ -12,6 +15,8 @@ const data = [
   },
   {
     name: "Floating Label for Input",
+    heading: "Floating Label for Input",
+    subheading: "The label for an input is animated, such that the label initially appears as a ",
     id: "floating_label",
     demoTab: <FLIDemo />,
     codeTab: <></>,
