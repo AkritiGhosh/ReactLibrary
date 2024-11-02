e6b6cebc19860173d45c127939d7677c32a8afe9
Akriti Ghosh <akritighosh98@gmail.com>
2024-11-02 22:20:40 +0530

    ui and size updated

--- src/topics/floating_label_input/Demo.jsx
+++ src/topics/floating_label_input/Demo.jsx
@@ -7,13 +7,13 @@ const FLIDemo = () => {
         <input
           type="text"
           required
-          className="w-full h-10 rounded-lg text-base px-2 bg-transparent border-2 border-black focus:border-teal-500 !outline-0 peer"
+          className="w-[40vw] h-14 rounded-lg text-xl leading-[3.5rem] px-4 bg-transparent border-2 border-black focus:border-teal-500 !outline-0 peer"
           id="floating_label_input"
         />
         <label
-          className="text-base absolute left-2 top-2/4 -translate-y-1/2 pointer-events-none duration-300 
+          className="text-xl absolute left-4 top-2/4 -translate-y-1/2 pointer-events-none duration-300 
           peer-focus:text-xs peer-focus:top-0 peer-focus:px-1 peer-focus:bg-white 
-          peer-valid:text-xs peer-valid:top-0 peer-valid:px-1 peer-valid:bg-white"
+          peer-valid:text-xs peer-valid:top-0 peer-valid:px-1 peer-valid:bg-white peer-valid:text-teal-500 peer-focus:text-teal-500"
           htmlFor="floating_label_input"
         >
           Enter value here
