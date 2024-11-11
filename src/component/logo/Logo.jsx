97890df615a157cf4e4410fef1c9c3c83a5053f0
Akriti Ghosh <akritighosh98@gmail.com>
2024-11-11 17:08:06 +0530

    mobile responsive layout & sidebar

--- src/component/logo/Logo.jsx
+++ src/component/logo/Logo.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import logoPath from "./logo.svg";
-import './logo.css'
+import "./logo.css";
 
-const Logo = () => {
-  return <img src={logoPath} className="logo animate-rotate" />;
+const Logo = ({ mobile }) => {
+  const w = mobile ? "w-7" : "";
+  return (
+    <img
+      src={logoPath}
+      className={`${w} logo animate-rotate`}
+    />
+  );
 };
 
 export default Logo;
--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,25 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../component/sidebar/Sidebar";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import data from "../data/data";
 import HeaderBlock from "../component/header/HeaderBlock";
+import SidebarMobile from "../component/sidebar/SidebarMobile";
 
 const MainLayout = () => {
   const nav = useNavigate();
   const page = useParams()?.page ?? false;
   const metadata = page ? data.filter((iter) => iter.id == page)[0] : data[0];
+  const pageWidth = screen.width;
+  const [mobileSidebar, setMobileSidebar] = useState(pageWidth < 1024);
+
+  useEffect(() => {
+    const handleResize = () => setMobileSidebar(screen.width < 1024);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   useEffect(() => {
     if (!page) nav(data[0].id, { replace: true });
   }, [page]);
 
   return (
     <>
-      <main className="w-screen h-screen flex flex-col lg:flex-row items-center justify-center">
+      <main className="w-screen h-screen flex flex-col lg:flex-row items-center justify-center bg-black">
         {/* Sidebar component */}
-        <div className="lg:w-1/5 w-full lg:h-screen h-auto lg:overflow-y-auto bg-black">
-          <Sidebar />
-        </div>
-        <div className="lg:w-4/5 grow w-full lg:h-screen h-auto overflow-y-auto relative scroll-smooth py-5 px-8">
+        {mobileSidebar ? (
+          <div className="w-full min-h-12 py-2 px-4 ">
+            <SidebarMobile />
+          </div>
+        ) : (
+          <div className="lg:w-1/5 min-w-64 w-full lg:h-screen h-auto lg:overflow-y-auto">
+            <Sidebar />
+          </div>
+        )}
+
+        <div className="lg:w-4/5 grow w-full lg:h-screen h-auto overflow-y-auto relative scroll-smooth py-5 px-8 border-black border-4 rounded-lg bg-white">
           <HeaderBlock data={metadata} />
           {/* Content component */}
           <Outlet />
