814411eb476ef369655e06d14752425f2b869e45
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-27 11:09:07 +0530

    logo - favicon updated

--- src/component/logo/Logo.jsx
+++ src/component/logo/Logo.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import logoPath from "./logo.svg";
+import logoPath from "/logo.svg";
 import "./logo.css";
 
 const Logo = ({ mobile }) => {
   const w = mobile ? "w-7" : "";
-  return (
-    <img
-      src={logoPath}
-      className={`${w} logo animate-rotate`}
-    />
-  );
+  return <img src={logoPath} className={`${w} logo animate-rotate`} />;
 };
 
 export default Logo;
